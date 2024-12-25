14497b37bc88153313d4524f101d3291382aeddb
張小凡 <2672931+whyb@users.noreply.github.com>
2024-12-25 17:54:34 +0800

    为验证模型加入胜率比对

--- val.py
+++ val.py
@@ -13,27 +13,45 @@ def validator():
     model2 = GomokuNetV2().to(device)
 
     # 加载模型权重
-    load_model_if_exists(model1, 'gobang_best_model.pth')
-    load_model_if_exists(model2, 'gobang_best_model.pth')
+    load_model_if_exists(model1, 'gobang_model_player1_12000.pth')
+    load_model_if_exists(model2, 'gobang_model_player1_9000.pth')
 
-    env.reset()
-    done = False
-    current_model = model1
+    player1_win_count = 0
+    player2_win_count = 0
 
-    while not done:
-        state = torch.FloatTensor(env.board.flatten()).to(device)
-        logits = current_model(state)
-        action = get_valid_action(logits.cpu().detach().numpy(), env.board)
-        if action == -1:
-            print("No valid actions available. Ending the game.")
-            break
-        reward, done = env.step(action)
-        if NEED_PRINT_BOARD:  # 打印中间状态
-            env.print_board()
-        if done:
-            print(f"Player {reward} win!")
-            break
-        current_model = model2 if current_model == model1 else model1
+    for round in range(1000):
+        env.reset()
+        done = False
+        current_model = model1
+
+        while not done:
+            state = torch.FloatTensor(env.board.flatten()).unsqueeze(0).to(device)  # 增加batch维度
+            if env.current_player == 1:
+                logits = model1(state)
+                action = get_valid_action(logits.cpu().detach().numpy(), env.board, 0.01)
+            else:
+                logits = model2(state)
+                action = get_valid_action(logits.cpu().detach().numpy(), env.board, 0.4)  # Player2 增加随机性
+
+            if action == -1:
+                print("No valid actions available. Ending the game.")
+                break
+            reward, done = env.step(action)
+            #if NEED_PRINT_BOARD:  # 打印中间状态
+                #env.print_board()
+            if done:
+                if reward == 1:
+                    player1_win_count += 1
+                elif reward == 2:
+                    player2_win_count += 1
+                total_game_count = player1_win_count + player2_win_count
+                player1_win_rate = (player1_win_count / total_game_count) * 100 if total_game_count > 0 else 0
+                player2_win_rate = (player2_win_count / total_game_count) * 100 if total_game_count > 0 else 0
+                print(f"Validator Round {round},\tPlayer {reward} win!\tPlayer 1 win rate: {player1_win_rate:.2f}%, Player 2 win rate: {player2_win_rate:.2f}%")
+                if NEED_PRINT_BOARD:
+                    env.print_board()
+                break
+            current_model = model2 if current_model == model1 else model1
 
 if __name__ == "__main__":
     validator()
